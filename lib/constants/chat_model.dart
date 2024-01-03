f61486684ae2f93a2f1d92dd75c075d0521dc32b
Sm69mu <soumyajitmukherjee271@gmail.com>
2024-01-03 23:39:51 +0530

    updated references

---
+++ lib/constants/chat_model.dart
@@ -0,0 +1,11 @@
+class ChatModels {
+  String name;
+  String icon;
+  String time;
+  String currentMessage;
+  ChatModels(
+      {required this.name,
+      required this.icon,
+      required this.currentMessage,
+      required this.time});
+}
